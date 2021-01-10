7f9a77b5c28b06b2af70d66ccb9462c3896e2d18
ZSendokame <70088953+ZSendokame@users.noreply.github.com>
2021-01-10 19:29:19 -0300

    Update USB-Roth.ps1

--- USB-Roth.ps1
+++ USB-Roth.ps1
@@ -12,7 +12,7 @@ Write-Output "|    |   /\_____  \  |    |  _/     |       _//  _ \ /  _ \   __\ 
 Write-Output "|    |  / /        \ |    |   \     |    |   (  <_> |  <_> )  | |   Y  \ "
 Write-Output "|______/ /_______  / |______  /     |____|_  /\____/ \____/|__| |___|  / "
 Write-Output "                 \/         \/             \/                        \/  "
-Write-Output " Ver: $Version                                           ZSendokame  "
+Write-Output " Ver: $Version                                           ZSendokame      "
 }
 
 Logo
