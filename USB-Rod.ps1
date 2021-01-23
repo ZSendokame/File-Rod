007ddb7e79590130cade6895cd7736914ca8b2dd
ZSendokame <70088953+ZSendokame@users.noreply.github.com>
2021-01-23 18:46:27 -0300

    Add files via upload

---
+++ USB-Rod.ps1
@@ -0,0 +1,54 @@
+$Username = $env:Username
+$Version = "1.0.4"
+$actualPath = Get-Location
+
+
+Clear-Host
+
+
+function Logo {
+
+Write-Output "   ____ ___  ___________________         __________           .___ "
+Write-Output "  |    |   \/   _____/\______   \        \______   \ ____   __| _/ "
+Write-Output "  |    |   /\_____  \  |    |  _/  ______ |       _//  _ \ / __ |  "
+Write-Output "  |    |  / /        \ |    |   \ /_____/ |    |   (  <_> ) /_/ |  "
+Write-Output "  |______/ /_______  / |______  /         |____|_  /\____/\____ |  "
+Write-Output "                   \/         \/                 \/            \/  "
+Write-Output " Ver: $Version                                       ZSendokame    "
+}
+
+Logo
+
+
+$Action = Read-Host "[$Username] ~ [Menu]>"
+
+
+
+if($Action -eq ".flash") {
+
+    $Path = Read-Host "[$Username] ~ [Flash]>"
+
+    Remove-Item $Path -Force
+
+
+} elseif($Action -eq ".backup") {
+
+    $backUpPath = Read-Host "[$Username] ~ [Original Path]"
+
+    $receiverPath = Read-Host "[$Username] ~ [Receiver Path]"
+
+    Set-Location $backUpPath
+
+    Copy-Item *.* $receiverPath
+
+} elseif($Action -eq ".getPath"){
+
+    $actualPath
+
+} elseif($Action -eq ".help"){
+
+    Write-Output ".help Shows this message", ".backup Backup your files", ".flash Delete files", ".getPath Shows the actual path"
+
+} else {
+    Write-Output "$Action its not recogniced"
+}
